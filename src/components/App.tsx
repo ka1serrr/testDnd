7436642783732a39c65a029b3f094fabbdeb41b7
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2023-04-15 14:04:49 +0300

    Deleted todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,6 @@ import { store } from '@/store/store';
 import { MainPage } from '@/components/page/MainPage';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-// TODO: короче нахуй, нужно всё переписать. В дату добавить type и подогнать всё под определенный массив. Создать один общий компонент (можно на базе DraggableComponent), содержание которого будет рендериться в зависимости от приходимого type.
 
 const App = () => {
   return (
